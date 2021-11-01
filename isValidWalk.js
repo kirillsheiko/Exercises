6c45bcbc24c4adb22e6650cd28ae3b4ad7bd4d52
kirillsheiko <kirillsheiko@gmail.com>
2021-11-02 01:01:56 +0300

    modified:   isValidWalk.js

--- isValidWalk.js
+++ isValidWalk.js
@@ -1,31 +1,22 @@
+const walk = ['n', 's', 'n', 's', 'n', 's', 'n', 's', 'n', 's'];
 function isValidWalk(walk) {
-  if (walk.lenght == 10 ){
-    let countE = 0;
-    let countW = 0;
-    let countN = 0;
-    let countS = 0;
-    for (let i = 0; i < walk.length; i++){
-      if (walk[i] == 'n'){
-        ++countN;
-      }
-      else if (walk[i] == 's'){
-        ++countS;
-      }
-      else if (walk[i] == 'e'){
-        ++countE;
-      }
-      else if (walk[i] == 'w'){
-        ++countW;
-      }
-    }
-    if (countE == countW & countN == countS){
+    const countN = walk.filter(function(char){
+      return char == "n";
+    });
+    const countS = walk.filter(function(char){
+      return char == "s";
+    });
+    const countW = walk.filter(function(char){
+      return char == "w";
+    });
+    const countE = walk.filter(function(char){
+      return char == "e";
+    });
+    if (countE.length == countW.length && countN.length == countS.length && walk.length == 10){
       return true;
     }
     else {
       return false;
     }
-  }
-  else {
-    return false;
-  }
-}
+    }
+isValidWalk(walk);
