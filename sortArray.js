a110dab743b320211367c1a3897c94bd880487c1
kirillsheiko <kirillsheiko@gmail.com>
2021-11-02 18:46:25 +0300

    new file:   sortArray.js

---
+++ sortArray.js
@@ -0,0 +1,18 @@
+let array = [5, 6, 1, 7, 8, 0, 22, 15];
+function sortArray(array){
+  const oddArray = array.filter(function(oddNumber){
+    return oddNumber % 2 == 1;
+  });
+  function compareNumbers(a,b){
+    return a - b;
+  }
+  oddArray.sort(compareNumbers);
+  let j = 0;
+  for (let i = 0; i < array.length; i++){
+    if (array[i] % 2 == 1){
+      array[i] = oddArray[j];
+      j++;
+    }
+  }
+return array;
+}
