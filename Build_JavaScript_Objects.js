9a4886d92b988ea9105a7671234e558c72207c67
kirillsheiko <kirillsheiko@gmail.com>
2021-11-13 12:46:44 +0300

    new file:   Build_JavaScript_Objects.js

---
+++ Build_JavaScript_Objects.js
@@ -0,0 +1,9 @@
+const myDog = {
+  // Only change code below this line
+  "name": "Muhtar",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["People", "wolfs"]
+
+  // Only change code above this line
+};
