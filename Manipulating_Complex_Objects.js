3b44e62484c498d0e0ada322cc03edd49f287204
kirillsheiko <kirillsheiko@gmail.com>
2021-11-14 14:57:50 +0300

    new file:   Manipulating_Complex_Objects.js

---
+++ Manipulating_Complex_Objects.js
@@ -0,0 +1,22 @@
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist": "ATL",
+    "title": "Limb",
+    "release_year": 2018,
+    "formats": [
+      "CD",
+      "LP"
+    ]
+  }
+];
